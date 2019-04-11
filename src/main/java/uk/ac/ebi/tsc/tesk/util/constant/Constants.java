8f99e87f58c2aaaa6b846cf6d964ab5251ea53de
Angelos Charalambidis <agcharal@gmail.com>
2019-04-11 10:52:51 +0300

    Increase to 12G

--- src/main/java/uk/ac/ebi/tsc/tesk/util/constant/Constants.java
+++ src/main/java/uk/ac/ebi/tsc/tesk/util/constant/Constants.java
@@ -123,7 +123,7 @@ public class Constants {
      */
     public static final String ABSOLUTE_PATH_MESSAGE = "must be an absolute path";
 
-    public static final double RESOURCE_DISK_DEFAULT = 10;
+    public static final double RESOURCE_DISK_DEFAULT = 12;
 
     /**
      * TES task states, indicating task is not running and cannot be cancelled
